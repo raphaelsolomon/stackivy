a7ea6722d0d8f5ff2b694e94d4ddc9f14638905e
Raphael Phoenix <phoenixk54@gmail.com>
2023-12-02 12:17:09 +0100

    Update main.dart
    title change

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Stackivy',
+      title: 'Stackivys',
       defaultTransition: Transition.cupertino,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
